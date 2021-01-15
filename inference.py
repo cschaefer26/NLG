3112e5772b8e4d5070524bc10c3af5c5f1b9ab80
cschaefer26 <c.schaefer.home@gmail.com>
2021-01-15 10:48:48 +0100

    Modified inference.

--- inference.py
+++ inference.py
@@ -1,18 +1,11 @@
-import re
-import json
-from transformers import TextDataset, DataCollatorForLanguageModeling
 from transformers import pipeline
-from transformers import AutoTokenizer
-from transformers import Trainer, TrainingArguments, AutoModelWithLMHead
-from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
-    tokenizer = AutoTokenizer.from_pretrained("anonymous-german-nlp/german-gpt2")
-    model = AutoModelWithLMHead.from_pretrained("anonymous-german-nlp/german-gpt2")
-    chef = pipeline(task='text-generation',
-                    model=model,
+
+    pipe = pipeline(task='text-generation',
+                    model='models/gpt2-model',
                     tokenizer='anonymous-german-nlp/german-gpt2',
                     config={'max_length': 800})
 
-    result = chef('Ein')[0]['generated_text']
+    result = pipe('Zuerst Knoblauch schneiden, dann')[0]['generated_text']
     print(result)
