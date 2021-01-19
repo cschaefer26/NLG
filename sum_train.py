7216cb318dd7f4e1d754ae1ebf5bc39b011bb9b0
cschaefer26 <c.schaefer.home@gmail.com>
2021-01-19 08:04:53 +0100

    Added summary.

--- sum_train.py
+++ sum_train.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import torch
 from typing import List
 
@@ -6,9 +8,11 @@ from torch.optim import AdamW
 import torch.nn.functional as F
 import pandas as pd
 from torch.utils.data import Dataset, RandomSampler, DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from transformers import AutoTokenizer, AutoModelWithLMHead
 from tqdm import tnrange, tqdm
 from torch.nn.utils.rnn import pad_sequence
+
 
 class SummarizerDataset(Dataset):
 
@@ -85,15 +89,18 @@ def generate(model, context, length, device, temperature=1, top_k=0, top_p=0.0):
 
 def train(model: AutoModelWithLMHead,
           train_dataset: Dataset,
-          test_dataset: Dataset,
+          val_dataset: Dataset,
           batch_size=16) -> None:
 
+    summary_writer = SummaryWriter('logs/gpt2-training')
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     model = model.to(device)
     train_sampler = RandomSampler(train_dataset)
     train_loader = DataLoader(dataset=train_dataset, sampler=train_sampler,
                               batch_size=batch_size, num_workers=0,
                               collate_fn=collate_dataset)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size,
+                            num_workers=0, collate_fn=collate_dataset)
     loss_func = CrossEntropyLoss(ignore_index=0, reduction='sum')
     optimizer = AdamW(model.parameters(), lr=2e-5)
     total_step = 0
@@ -117,9 +124,28 @@ def train(model: AutoModelWithLMHead,
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), 1.)
             optimizer.step()
-            if total_step % 10 == 0:
-                print(f'\nepoch {epoch} step {total_step}, loss: {loss}')
+            summary_writer.add_scalar('Loss/train', loss, global_step=total_step)
 
+            # EVALUATION
+            if total_step % 10 == 0:
+                model.eval()
+                val_loss = 0
+                val_norm = 0
+                for val_batch in val_loader:
+                    inputs, labels = batch['tokens'].to(device), batch['tokens'].to(device)
+                    with torch.no_grad():
+                        logits = model(inputs)[0]
+                    for b, idx in enumerate(val_batch['abstract_len']):
+                        shift_logits = logits[b:b + 1, idx:-1, :]
+                        shift_labels = labels[b:b + 1, idx + 1:]
+                        b_loss = loss_func(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+                        val_loss += b_loss
+                        val_norm += shift_labels.size(1)
+                val_loss = val_loss / val_norm
+                summary_writer.add_scalar('Loss/val', val_loss, global_step=total_step)
+                model.train()
+
+            # GENERATION
             texts = [
                 'Deutsche Bank sehr schwach nach Aussagen zum Konzernumbau',
                 'Mann nach Sturz in Brunnen schwer verletzt',
@@ -127,24 +153,34 @@ def train(model: AutoModelWithLMHead,
                 'Bayern verliert klar im Pokalfinale gegen Liverpool'
             ]
             if total_step % 100 == 0:
+                model.eval()
                 for text in texts:
                     inp = tokenizer.encode(text) + tokenizer.encode('|')
                     gen = generate(model, context=inp, length=100, device=device)
                     gen = tokenizer.decode(gen[0])
                     torch.save(model.state_dict(), 'models/gpt2-checkpoint.pt')
                     print(f'step {step}, gen: {gen}')
+                model.train()
     return None
 
 
 if __name__ == '__main__':
+    Path('logs').mkdir(exist_ok=True, parents=True)
+    Path('models').mkdir(exist_ok=True, parents=True)
     tokenizer = AutoTokenizer.from_pretrained('dbmdz/german-gpt2')
-    df = pd.read_csv('data/train.csv')
-    dataset = SummarizerDataset(abstracts=df['seoTitle'].tolist(),
-                                      texts=df['body'].tolist(),
+    df_train = pd.read_csv('data/train.csv')
+    train_dataset = SummarizerDataset(abstracts=df_train['seoTitle'].tolist(),
+                                      texts=df_train['body'].tolist(),
+                                      tokenizer=tokenizer)
+    df_test = pd.read_csv('data/test.csv')
+    test_dataset = SummarizerDataset(abstracts=df_test['seoTitle'].tolist()[:10],
+                                      texts=df_test['body'].tolist()[:10],
                                       tokenizer=tokenizer)
 
     model = AutoModelWithLMHead.from_pretrained('dbmdz/german-gpt2')
-    train(model, dataset, dataset)
+    #state_dict = torch.load('models/gpt2-checkpoint', map_location=torch.device('cpu'))
+    #model.load_state_dict(state_dict)
+    train(model, train_dataset, test_dataset)
     #gen = generate(model, tokenizer.encode('Das Wetter ist heute sonnig,'),
     #               length=10, device=torch.device('cpu'), top_k=5)
     #print(tokenizer.decode(gen[0]))
